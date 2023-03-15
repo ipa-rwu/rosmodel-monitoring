067d7fbe846b64cbedcbba811ad755a3ec891cad
Ruichao Wu <ruichao.wu@ipa.fraunhofer.de>
2023-03-15 18:03:40 +0100

    install developed python package

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [ ! -d venv ]; then
+    for p in "py -3.8" "py -3" "python3.8" "python3" "python"; do
+        if $p -V &> /dev/null; then
+            pycmd=$p
+            break
+        fi
+    done
+    ${pycmd:?Could not determine python executable} -mvenv venv
+fi
+
+venv/*/pip install --find-links wheels -e textx-pyecoregen
+venv/*/pip install --find-links wheels -e pythonic-rosmodel
