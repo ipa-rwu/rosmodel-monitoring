f88bf9d707f5e1ca68d9f13bacdf158b6c622fde
Ruichao Wu <ruichao.wu@ipa.fraunhofer.de>
2023-03-16 10:42:18 +0100

    add arg metamodel repository when generating metamodel from ecore

--- README.md
+++ README.md
@@ -6,10 +6,15 @@
 python3 -m venv venv
 ```
 ```
+# import git submodule
+git submodule update
+```
+```
 # install packages
 ./install.sh
 ```
 ```
-# regenerate rosmodel from ecore: ./gen_metamodel_pyecore.sh "branch name of https://github.com/ipa-nhg/ros-model" "folder"
-./gen_metamodel_pyecore.sh
+# regenerate rosmodel from ecore:
+# ./gen_metamodel_pyecore.sh "repo name" "branch name of https://github.com/ipa-nhg/ros-model" "folder"
+./gen_metamodel_pyecore.sh ipa-rwu/ros-model rwu/fix/YamlMigrationSystem pythonic-rosmodel/pythonic_rosmodel/metamodel_gen
 ```
--- gen_metamodel_pyecore.sh
+++ gen_metamodel_pyecore.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
+METAMODEL_REPO=ipa-nhg/ros-model
 METAMODEL_BRANCH=YamlMigrationSystem
 METAMODEL_FOLDER=pythonic-rosmodel/pythonic_rosmodel/metamodel_gen
 
-METAMODEL_BRANCH=${1:-$METAMODEL_BRANCH}
-METAMODEL_FOLDER=${2:-$METAMODEL_FOLDER}
+METAMODEL_REPO=${1:-$METAMODEL_REPO}
+METAMODEL_BRANCH=${2:-$METAMODEL_BRANCH}
+METAMODEL_FOLDER=${3:-$METAMODEL_FOLDER}
 
 function log_error {
     local out; out=$(mktemp)
@@ -24,11 +26,11 @@ function run_python {
 
 }
 
-run_python -m textx_pyecoregen.cli --ecore-model https://raw.githubusercontent.com/ipa-nhg/ros-model/"$METAMODEL_BRANCH"/plugins/de.fraunhofer.ipa.ros/model/ros.ecore -o "$METAMODEL_FOLDER"  --auto-register-package --with-dependencies
+run_python -m textx_pyecoregen.cli --ecore-model https://raw.githubusercontent.com/"$METAMODEL_REPO"/"$METAMODEL_BRANCH"/plugins/de.fraunhofer.ipa.ros/model/ros.ecore -o "$METAMODEL_FOLDER"  --auto-register-package --with-dependencies
 
 # fix python module name
-sed -i 's/^from ros/from pythonic_rosmodel.metamodel_gen.ros/' pythonic-rosmodel/pythonic_rosmodel/metamodel_gen/primitives/__init__.py
 sed -i 's/^from primitives/from pythonic_rosmodel.metamodel_gen.primitives/' pythonic-rosmodel/pythonic_rosmodel/metamodel_gen/ros/__init__.py
+sed -i 's/^from primitives/from pythonic_rosmodel.metamodel_gen.primitives/' pythonic-rosmodel/pythonic_rosmodel/metamodel_gen/ros/ros.py
 sed -i 's/^from type/from pyecore.type/' pythonic-rosmodel/pythonic_rosmodel/metamodel_gen/ros/ros.py
 
 isort pythonic-rosmodel/pythonic_rosmodel/metamodel_gen
